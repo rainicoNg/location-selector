4619816635ca9d190d026d98fab91d8b06a840d7
rainicoying <rainicong@magicubedu.com>
2024-10-12 00:54:28 +0800

    Added - breakpoint setting

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,27 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-    content: [
-        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    ],
-    theme: {
-        extend: {
-            colors: {
-                background: "var(--background)",
-                foreground: "var(--foreground)",
-                primary: "var(--primary)",
-                dark: "var(--dark)",
-                disabled: "var(--disabled)",
-            },
-        },
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        primary: "var(--primary)",
+        dark: "var(--dark)",
+        disabled: "var(--disabled)"
+      }
     },
-    plugins: [],
+    screens: {
+      tablet: "768px",
+      laptop: "1024px",
+      desktop: "1280px"
+    }
+  },
+  plugins: []
 };
 export default config;
