255b62325dfffbe7f30b42369555608e6d903a1b
rainicoying <rainicong@magicubedu.com>
2024-10-12 00:55:26 +0800

    Updated - props interface

--- src/app/components/ResultContainer/index.tsx
+++ src/app/components/ResultContainer/index.tsx
@@ -1,11 +1,15 @@
 import { IPathDetails } from "@/app/page";
 
-export default function ResultContainer({...props}: IPathDetails) {
+interface IResultContainerProps {
+  pathDetails: IPathDetails;
+  className?: string;
+}
+export default function ResultContainer({...props}: IResultContainerProps) {
   return (
-    <div className="border rounded">
-      <h4 className={props.status}>{props.message}</h4>
-      {props.totalDistance && <h5>Total Distance: {props.totalDistance}</h5>}
-      {props.totalTime && <h5>Total Time: {props.totalTime}</h5>}
+    <div className={`border rounded ${props.className}`}>
+      <h4 className={props.pathDetails.status}>{props.pathDetails.message}</h4>
+      {props.pathDetails.totalDistance && <h5>Total Distance: {props.pathDetails.totalDistance}</h5>}
+      {props.pathDetails.totalTime && <h5>Total Time: {props.pathDetails.totalTime}</h5>}
     </div>
   )
 }
