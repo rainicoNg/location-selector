70bfb9920f00537455c3cf0086a2c2a1766ad4f5
rainicoying <rainicong@magicubedu.com>
2024-10-12 20:37:23 +0800

    Added - driving route on map container

--- src/app/components/MapContainer/index.tsx
+++ src/app/components/MapContainer/index.tsx
@@ -1,49 +1,90 @@
-import {APIProvider, Map, Marker} from '@vis.gl/react-google-maps';
-import { useEffect, useState } from 'react';
+import { APIProvider, Map, useMapsLibrary, useMap } from "@vis.gl/react-google-maps";
+import { useEffect, useState } from "react";
+import LocationPointer from "../LocationPointer";
+
+function Routes({wayPoints}: {wayPoints: google.maps.LatLngLiteral[]}) {
+  const map = useMap(process.env.NEXT_PUBLIC_GOOGLE_MAP_ID);
+  const routeLib = useMapsLibrary("routes");
+  const [dirService, useDirService] = useState<google.maps.DirectionsService>();
+  const [dirRenderer, useDirRenderer] = useState<google.maps.DirectionsRenderer>();
+
+  useEffect(() => {
+    if (!routeLib || !map) return;
+      useDirService(new routeLib.DirectionsService());
+      useDirRenderer(new routeLib.DirectionsRenderer({ map, suppressMarkers: true }));
+  }, [routeLib, map]);
+
+  useEffect(() => {
+    if (!dirService || !dirRenderer) return;
+
+    if (wayPoints && wayPoints.length > 2) {
+      // set map if not set
+      if (dirRenderer.getMap() === null && map){
+        dirRenderer.setMap(map);
+      }
+      // draw driving route
+      dirService.route({
+        origin: wayPoints[0],
+        destination: wayPoints[wayPoints.length - 1],
+        travelMode: google.maps.TravelMode.DRIVING,
+        optimizeWaypoints: false,
+        waypoints: wayPoints.map(r => ({location: r})),
+      }).then(res => {
+        dirRenderer.setDirections(res);
+      }).catch((e) => {
+        console.error(e);
+      });
+    } else {
+      // clear route if no way points
+      dirRenderer.setMap(null);
+    }
+  }, [wayPoints]);
+  return <></>;
+}
 
 interface IMapContainerProps {
   path?: string[][];
   className?: string;
 }
+
 export default function MapContainer({...props}: IMapContainerProps) {
-  const [paths, usePaths] = useState<string[][]>([]);
-  const [center, useCenter] = useState({lat: 22.33560, lng: 114.17611});
-  const [zoom, useZoom] = useState(10);
+  const [paths, usePaths] = useState<google.maps.LatLngLiteral[]>([]);
 
   useEffect(() => {
-    if (props.path && props.path.length > 0) {
-      usePaths(props.path.filter(p => !(Number.isNaN(Number.parseFloat(p[0])) && Number.isNaN(Number.parseFloat(p[1])))));
+    if (props.path) {
+      usePaths(props.path.map(p => ({
+        lat: Number.parseFloat(p[0]),
+        lng: Number.parseFloat(p[1])
+      })).filter(p => !(Number.isNaN(p.lat) || Number.isNaN(p.lng))));
+    } else {
+      usePaths([]);
     }
-  }, [props.path])
-
-  useEffect(() => {
-    if (paths.length > 0) {
-      const lat = paths.reduce((acc, cur) => acc + Number.parseFloat(cur[0]), 0) / paths.length;
-      const lng = paths.reduce((acc, cur) => acc + Number.parseFloat(cur[1]), 0) / paths.length;
-      useCenter({lat, lng});
-      useZoom(12);
-    }
-  }, [paths])
+  }, [props.path]);
 
   return (
-    <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY!}>
-      <Map
-        style={{width: "100vw", height: "100vh"}}
-        center={center}
-        zoom={zoom}
-        gestureHandling={'greedy'}
-        disableDefaultUI={true}
-        className={props.className}
-      >
-        {paths && paths.length > 0 && paths.map((p, i) => 
-          (
-            // TODO: use custom pointer
-            <Marker
-              position={{lat: Number.parseFloat(p[0]), lng: Number.parseFloat(p[1])}}
-            />
-          )
-        )}
-      </Map>
-    </APIProvider>
+    <div className={props.className}>
+      <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY!}>
+        <Map
+          mapId={process.env.NEXT_PUBLIC_GOOGLE_MAP_ID}
+          style={{width: "100%", height: "100%"}}
+          defaultCenter={{lat: 22.33560, lng: 114.17611}}
+          defaultZoom={12}
+          gestureHandling="greedy"
+          disableDefaultUI={true}
+        >
+          <Routes wayPoints={paths}/>
+          {paths && paths.length > 0 && paths.map((p, i) => 
+            (
+              <LocationPointer 
+                key={i} 
+                lat={p.lat} 
+                lng={p.lng} 
+                index={i+1} 
+              />
+            )
+          )}
+        </Map>
+      </APIProvider>
+    </div>
   );
 }
