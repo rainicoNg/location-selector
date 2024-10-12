9e3c9fe7b7ecbc0bb50431b66d8fac3802ff3de3
rainicoying <rainicong@magicubedu.com>
2024-10-12 16:19:47 +0800

    Updated - move icon into input, input styling

--- src/app/components/LocationInput/index.tsx
+++ src/app/components/LocationInput/index.tsx
@@ -10,26 +10,27 @@ interface ILocationInputProps {
   onInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   onLocationDelete: () => void;
   value?: string;
+  className?: string;
 }
 
 export default function LocationInput({...props}: ILocationInputProps) {
   return (
-    <div>
-      <label>{props.label}</label>
-      <div>
+    <div className={props.className}>
+      <label className="font-semibold">{props.label}</label>
+      <div className="w-full flex relative">
+        <IconButton
+          onClick={props.onLocationDelete}
+          icon={faTrash}
+          disabled={!props.deletable}
+          className="text-negative absolute inset-y-0 right-0"
+        />
         <input
           type="text"
           name={props.name}
           value={props.value || ""}
           onChange={props.onInputChange}
-          className="border border-gray-300 rounded"
+          className="border border-gray-400 rounded grow py-1 pl-2 pr-6"
         />
-        <IconButton
-          onClick={props.onLocationDelete}
-          icon={faTrash}
-          disabled={!props.deletable}
-          className="text-red-400"
-        ></IconButton>
       </div>
     </div>
   );
